c5661568e270e50da5bc6d604a8b547ad3b0715a
administrador23 <87953444+WilliamCipriani@users.noreply.github.com>
2023-09-06 23:41:57 -0500

    Se agegro a la web la parte de Preguntas Frecuente

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,17 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer class="w-full min-h-[28rem] flex items-center justify-center bg-black">
-            <div class="md:w-2/3 w-full px-6 text-white flex flex-col">
-                <div class="w-full text-7xl font-bold">
-                    <h1 class="w-full md:w-2/3">¡Ponerse en contacto!</h1>
+        <footer className="w-full min-h-[28rem] flex items-center justify-center bg-black">
+            <div className="md:w-2/3 w-full px-6 text-white flex flex-col">
+                <div className="w-full text-7xl font-bold">
+                    <h1 className="w-full md:w-2/3">¡Ponerse en contacto!</h1>
                 </div>
-                <div class="flex mt-8 flex-col md:flex-row md:justify-between">
-                    <p class="w-full md:w-2/3 text-gray-400">
+                <div className="flex mt-8 flex-col md:flex-row md:justify-between">
+                    <p className="w-full md:w-2/3 text-gray-400">
                         Descubre cómo nuestros productos pueden ayudarle a ser más productivo.
                     </p>
-                    <div class="w-44 pt-6 md:pt-0">
-                        <a href="#" class="bg-red-500 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center">
+                    <div className="w-44 pt-6 md:pt-0">
+                        <a href="#" className="bg-red-500 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center">
                             Contacto
                         </a>
                     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer"
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [openSection, setOpenSection] = useState(null);
+
+  const toggleSection = (section) => {
+    setOpenSection(openSection === section ? null : section);
+  };
+
   return (
     <>
       <Header />
@@ -44,19 +51,21 @@ export default function Home() {
             <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
               Sobre Nosotros
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+            <p className="mt-6 text-lg leading-8 text-gray-300 text-justify">
+              En NA, estamos en el emocionante viaje de la innovación y la
+              excelencia en consultoría. Somos una empresa joven y apasionada,
+              dedicada a ofrecer soluciones de vanguardia en una amplia gama de
+              servicios, incluyendo desarrollo web, desarrollo de software,
+              análisis de datos, gestión de data centers y implementación de
+              arquitectura de redes. Nuestro compromiso es impulsar tu negocio
+              hacia el éxito digital, brindándote servicios que están a la
+              vanguardia de la tecnología y adaptados a tus necesidades
+              específicas. Nos enorgullecemos de trabajar estrechamente con
+              nuestros clientes para comprender sus metas y desafíos, y luego
+              diseñar soluciones personalizadas que impulsen su crecimiento y
+              eficiencia.
             </p>
           </div>
-          
         </div>
       </div>
 
@@ -66,62 +75,242 @@ export default function Home() {
         </h1>
 
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-3 lg:grid-cols-3 ">
-            <div className="w-full ">
-                <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse" >
-                  <a href="#">
-                    <img src="/image/analisis-datos.jpg" className="h-full w-full object-cover"/>
-                  </a>
-                </div>
-                
-                <h1 className="text-2xl w-full h-2 mt-4 text-center"> Análisis de Datos</h1>
-                <p className="w-24 h-2 mt-4 "></p>
+          <div className="w-full ">
+            <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
+              <a href="#">
+                <img
+                  src="/image/analisis-datos.jpg"
+                  className="h-full w-full object-cover"
+                />
+              </a>
             </div>
 
-            <div className="w-full "> 
-                <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
-                  <a href="#">
-                    <img src="/image/web.jpg" className="h-full w-full object-cover"/>
-                  </a> 
-                </div>
-                <h1 className="text-2xl w-full h-2 mt-4 text-center">Desarrollo Web</h1>
-                <p className="w-24 h-2 mt-4 "></p>
+            <h1 className="text-2xl w-full h-2 mt-4 text-center">
+              {" "}
+              Análisis de Datos
+            </h1>
+            <p className="w-24 h-2 mt-4 "></p>
+          </div>
+
+          <div className="w-full ">
+            <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
+              <a href="#">
+                <img
+                  src="/image/web.jpg"
+                  className="h-full w-full object-cover"
+                />
+              </a>
+            </div>
+            <h1 className="text-2xl w-full h-2 mt-4 text-center">
+              Desarrollo Web
+            </h1>
+            <p className="w-24 h-2 mt-4 "></p>
+          </div>
+
+          <div className="w-full ">
+            <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
+              <a href="#">
+                <img
+                  src="/image/data-center.jpg"
+                  className="h-full w-full object-cover"
+                />
+              </a>
             </div>
 
-            <div className="w-full ">
-                <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
-                  <a href="#">
-                    <img src="/image/data-center.jpg" className="h-full w-full object-cover"/>
-                  </a>
-                </div>
-                
-                <h1 className="text-2xl w-full h-2 mt-4 text-center">Data Center</h1>
-                <p className="w-24 h-2 mt-4 "></p>
+            <h1 className="text-2xl w-full h-2 mt-4 text-center">
+              Data Center
+            </h1>
+            <p className="w-24 h-2 mt-4 "></p>
+          </div>
+
+          <div className="w-full ">
+            <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
+              <a href="#">
+                <img
+                  src="/image/redes.jpg"
+                  className="w-full h-full object-cover"
+                />
+              </a>
             </div>
 
-            <div className="w-full ">
-                <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse">
-                  <a href="#">
-                    <img src="/image/redes.jpg" className="w-full h-full object-cover"/>
-                  </a>
-                </div>
-                
-                <h1 className="text-2xl w-full h-2 mt-4 text-center">Arquitectura de Redes</h1>
-                <p className="w-24 h-2 mt-4"></p>
+            <h1 className="text-2xl w-full h-2 mt-4 text-center">
+              Arquitectura de Redes
+            </h1>
+            <p className="w-24 h-2 mt-4"></p>
+          </div>
+
+          <div className="w-full ">
+            <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse ">
+              <a href="#">
+                <img
+                  src="/image/software.jpg"
+                  className="h-full w-full object-cover"
+                />
+              </a>
             </div>
 
-            <div className="w-full ">
-                <div className="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 animate-pulse ">
-                  <a href="#">
-                    <img src="/image/software.jpg" className="h-full w-full object-cover"/>
-                  </a>
-                </div>
-                
-                <h1 className="text-2xl w-full h-2 mt-4 text-center">Desarrollo de Software</h1>
-                <p className="w-24 h-2 mt-4 "></p>
-            </div>
+            <h1 className="text-2xl w-full h-2 mt-4 text-center">
+              Desarrollo de Software
+            </h1>
+            <p className="w-24 h-2 mt-4 "></p>
+          </div>
         </div>
-    </div>
-    <Footer />
+      </div>
+
+      <div
+        id="accordion-collapse"
+        data-accordion="collapse"
+        className="max-w-xl mx-auto py-10 "
+      >
+        <h1 className="text-4xl font-bold tracking-tight text-black sm:text-6xl text-center py-6">
+          Preguntas frecuentes
+        </h1>
+        <h2 id="accordion-collapse-heading-1">
+          <button
+            type="button"
+            onClick={() => toggleSection(1)}
+            className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+            data-accordion-target="#accordion-collapse-body-1"
+            aria-expanded="true"
+            aria-controls="accordion-collapse-body-1"
+          >
+            <span>¿Tienen experiencia en proyectos?</span>
+            <svg
+              data-accordion-icon
+              className="w-3 h-3 rotate-180 shrink-0"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5 5 1 1 5"
+              />
+            </svg>
+          </button>
+        </h2>
+        <div
+          id="accordion-collapse-body-1"
+          className={openSection === 1 ? "" : "hidden"}
+          aria-labelledby="accordion-collapse-heading-1"
+        >
+          <div className="p-5 border border-b-0 border-gray-200 ">
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              Sí, hemos trabajado con una variedad de clientes en diversos
+              sectores, incluyendo desarrollo web, desarrollo de software,
+              análisis de datos y más. Nuestra experiencia nos ha permitido
+              abordar una amplia gama de desafíos y adaptar nuestras soluciones
+              a las necesidades específicas de cada cliente. Estamos seguros de
+              que podemos aplicar nuestro conocimiento y experiencia para
+              cumplir con tus requisitos y objetivos de manera efectiva.
+            </p>
+          </div>
+        </div>
+        <h2 id="accordion-collapse-heading-2">
+          <button
+            type="button"
+            onClick={() => toggleSection(2)}
+            className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+            data-accordion-target="#accordion-collapse-body-2"
+            aria-expanded="false"
+            aria-controls="accordion-collapse-body-2"
+          >
+            <span>¿Cuál es su enfoque de precios y tarifas?</span>
+            <svg
+              data-accordion-icon
+              className="w-3 h-3 rotate-180 shrink-0"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5 5 1 1 5"
+              />
+            </svg>
+          </button>
+        </h2>
+        <div
+          id="accordion-collapse-body-2"
+          className={openSection === 2 ? "" : "hidden"}
+          aria-labelledby="accordion-collapse-heading-2"
+        >
+          <div className="p-5 border border-b-0 border-gray-200">
+            <p className="mb-2 text-gray-500 ">
+              Nuestro enfoque de precios y tarifas se adapta a las necesidades
+              de nuestros clientes. Utilizamos un enfoque personalizado en el
+              que evaluamos la complejidad del proyecto, el alcance de trabajo
+              requerido y otros factores relevantes para proporcionarte una
+              cotización precisa y competitiva. Trabajamos en estrecha
+              colaboración contigo para comprender tu presupuesto y garantizar
+              que nuestras tarifas sean transparentes y justas. Además,
+              ofrecemos diferentes opciones de estructuración de precios, como
+              proyectos por hora o tarifas fijas, para satisfacer tus
+              necesidades específicas.
+            </p>
+          </div>
+        </div>
+        <h2 id="accordion-collapse-heading-3">
+          <button
+            type="button"
+            onClick={() => toggleSection(3)}
+            className="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+            data-accordion-target="#accordion-collapse-body-3"
+            aria-expanded="false"
+            aria-controls="accordion-collapse-body-3"
+          >
+            <span>¿Cómo podemos poner en marcha un proyecto con ustedes?</span>
+            <svg
+              data-accordion-icon
+              className="w-3 h-3 rotate-180 shrink-0"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 5 5 1 1 5"
+              />
+            </svg>
+          </button>
+        </h2>
+        <div
+          id="accordion-collapse-body-3"
+          className={openSection === 3 ? "" : "hidden"}
+          aria-labelledby="accordion-collapse-heading-3"
+        >
+          <div className="p-5 border border-t-0 border-gray-200">
+            <p className="mb-2 text-gray-500">
+              Iniciar un proyecto con nosotros es un proceso sencillo y
+              eficiente. El primer paso es ponerse en contacto con nosotros a
+              través de nuestro formulario de contacto o por correo electrónico,
+              donde puedes proporcionarnos detalles sobre tu proyecto y tus
+              objetivos. A partir de ahí, programaremos una consulta inicial
+              gratuita en la que discutiremos tus necesidades en detalle y
+              responderemos a cualquier pregunta que puedas tener. Después de la
+              consulta inicial, desarrollaremos un plan de trabajo y una
+              estrategia personalizada para tu proyecto, que incluirá un
+              cronograma y una cotización detallada. Una vez que estés
+              satisfecho con nuestra propuesta, firmaremos un contrato y
+              comenzaremos a trabajar en tu proyecto de inmediato.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </>
   );
 }
