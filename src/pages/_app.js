000f14546594cd128c0055dbc7bda3ddcb1b4aa9
administrador23 <87953444+WilliamCipriani@users.noreply.github.com>
2023-11-15 17:55:41 -0500

    correcion del 404 prueba1

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,32 +1,16 @@
 import '@/styles/globals.css'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import {useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Head from 'next/head';
+
 
 export default function App({ Component, pageProps }) {
 
-  useEffect(() => {
-    AOS.init({
-      duration: 2000,
-      once: true,
-    });
-  }, []);
+ 
 
   return (
     <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link 
-          src="https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.14.3/react-modal.min.js"
-          integrity="sha512-MY2jfK3DBnVzdS2V8MXo5lRtr0mNRroUI9hoLVv2/yL3vrJTam3VzASuKQ96fLEpyYIT4a8o7YgtUs5lPjiLVQ=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-          />
-      </Head>
       <Component {...pageProps} />
     </>
   )
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="es">
       <Head >
         <link rel="icon" href="/image/index/an-servicios-tecnologicos-logo-head.png"/>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body>
         <Main />
